01ee702b6d161e897499a46ce77e0914e2aa60d7
Dion Cushion <dioncushion@hotmail.com>
2024-05-23 13:19:34 +1000

    docs: modify text for functions

--- BikeDataProject.py
+++ BikeDataProject.py
@@ -59,7 +59,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day if applicable. Valid city and valid answer required.
 
     Args:
         (str) city - name of the city to analyze
